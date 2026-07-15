007878d4b1ad9a0252bf59b00f8e9294c753b88c
Sourodeep Saha <sourodeepsaha28@gmail.com>
2026-07-15 20:27:53 +0530

    feat: orchestrate analytics page state and routing

--- src/pages/Analytics/Analytics.jsx
+++ src/pages/Analytics/Analytics.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import { Grid } from "@mui/material";
+
 import MainLayout from "../../components/layout/MainLayout";
-import { Typography } from "@mui/material";
+import AnalyticsHeader from "./AnalyticsHeader";
+import AnalyticsStats from "./AnalyticsStats";
+import TrafficChart from "./TrafficChart";
+import DevicesChart from "./DevicesChart";
+import TopPages from "./TopPages";
 
 const Analytics = () => {
+  const [dateRange, setDateRange] = useState("7d");
+
   return (
     <MainLayout>
-      <Typography variant="h3">
-        Analytics
-      </Typography>
+      <AnalyticsHeader dateRange={dateRange} onDateRangeChange={setDateRange} />
+
+      <AnalyticsStats />
+
+      <Grid container spacing={3}>
+        <Grid size={{ xs: 12, lg: 8 }}>
+          <TrafficChart dateRange={dateRange} />
+        </Grid>
+
+        <Grid size={{ xs: 12, lg: 4 }}>
+          <DevicesChart />
+        </Grid>
+
+        <Grid size={{ xs: 12 }}>
+          <TopPages />
+        </Grid>
+      </Grid>
     </MainLayout>
   );
 };
